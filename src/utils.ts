b3f991f8e2281c0747b883c23e24d254790ab1f0
Aman Kumar <akumar30@atlassian.com>
2024-02-29 22:36:43 +0530

    Prune refs of deleted remote branches

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,8 @@
 import { execSync } from 'child_process';
 
+export function pruneRemoteBranchRef() {
+    execSync('git fetch origin --prune');
+}
 export function getLocalBranches(): string[] {
     const rawLocalBranches = execSync('git branch').toString().trim().split('\n');
     const processedBranchNames = rawLocalBranches.map(branch => branch.replace('*', '').trim());
@@ -7,6 +10,9 @@ export function getLocalBranches(): string[] {
 }
 
 export function getRemoteBranches(): string[] {
+    // If we don't prune refs of deleted remote branches locally
+    // Our local repo will still think these branches exits on remote
+    pruneRemoteBranchRef();
     const rawRemoteBranches = execSync('git branch -r').toString().trim().split('\n');
     const processedBranchNames = rawRemoteBranches.map(branch => branch.replace('origin/', '').trim());
     return processedBranchNames;
